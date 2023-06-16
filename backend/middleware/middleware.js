935816988cbd123e88116c461a190f950ce697d3
leo <leandroadames12345@gmail.com>
2023-06-17 01:30:48 +0900

    sum

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -5,7 +5,21 @@ import signupRouter from "../routes/signupRoutes.js";
 import loginRouter from "../routes/loginRoutes.js";
 import myListRouter from "../routes/myListRoutes.js";
 import { hashPasswordMiddleware } from "../auth.js";
+import pg from "pg";
 dotenv.config();
+
+
+
+const db = new pg.Pool({
+    // connectionString: process.env.DATABASE_URL,
+    user: "leo",
+    host: "localhost",
+    database: 'todo',
+    password: "Uglyg00d",
+    port: "5432"
+});
+
+db.connect();
 
 const app = express();
 
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,21 @@
 import app from "./middleware/middleware.js";
 import dotenv from "dotenv";
 import postgres from "postgres";
+import pg from "pg";
 
 dotenv.config();
+
+const db = new pg.Pool({
+  // connectionString: process.env.DATABASE_URL,
+  user: "leo",
+  host: "localhost",
+  database: 'movies',
+  password: "Uglyg00d",
+  port: "5432"
+});
+
+db.connect();
+
 
 export const sql = postgres(process.env.DATABASE_URL);
 
