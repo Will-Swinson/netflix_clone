a9bec6778b0cba9d275ed2534dc703cd8d35a149
Will Swinson <122845015+Will-Swinson@users.noreply.github.com>
2023-06-23 16:17:38 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import MovieProvider from "./context/MovieProvider.jsx";
+import dotenv from "dotenv";
 
+dotenv.config({path: ../.env});
+  
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env}>
+    <BrowserRouter basename={process.env.BASENAME}>
       <MovieProvider>
         <App />
       </MovieProvider>
