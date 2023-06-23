d7fddb0d12d649dce5c1fc85d66fa6b5232daac6
Will-Swinson <william.swinson01@gmail.com>
2023-06-22 21:43:24 -0500

    Added Responsive header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getAllMovieAndShows } from "./utils/getMovieData.jsx";
 import { useMovies } from "./context/MovieProvider.jsx";
+import { useNavigate } from "react-router-dom";
 import "tailwindcss/tailwind.css";
 import LandingPage from "./pages/LandingPage.jsx";
 import SignInPage from "./pages/SignInPage.jsx";
@@ -20,6 +21,7 @@ import "./App.css";
 import NetflixProfile from "./components/NetflixProfile.jsx";
 function App() {
   const { setMovies } = useMovies();
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Fetch data from API
@@ -28,10 +30,30 @@ function App() {
     });
   }, []);
 
+  // Check if user is logged in
+  useEffect(() => {
+    const currentUser =
+      sessionStorage.getItem("token") !== null
+        ? JSON.parse(sessionStorage.getItem("token"))
+        : sessionStorage.clear();
+
+    if (
+      !currentUser &&
+      window.location.pathname !== "/signin" &&
+      window.location.pathname !== "/signup" &&
+      window.location.pathname !== "/signup2" &&
+      window.location.pathname !== "/signup3" &&
+      window.location.pathname !== "/signup4" &&
+      window.location.pathname !== "/payment" &&
+      window.location.pathname !== "/"
+    ) {
+      navigate("/");
+    }
+    console.log(currentUser);
+  }, [navigate]);
+
   return (
     <>
-
-
       <Routes>
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPlanPage1 />} />
@@ -40,10 +62,10 @@ function App() {
         <Route path="/signup4" element={<SignUpPlanPage4 />} />
         <Route path="/payment" element={<SignUpPlanPaymentPage />} />
         <Route path="/home" element={<MovieHomePage />} />
+        <Route path="/my-list" element={<MyListPage />} />
         <Route path="/profile-login" element={<NetflixProfile />} />
         <Route path="/" element={<LandingPage />} />
       </Routes>
-
     </>
   );
 }
