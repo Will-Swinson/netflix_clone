0173ab3e906158c46ca04c622098513db46b6fc6
Will-Swinson <william.swinson01@gmail.com>
2023-06-25 00:29:53 -0500

    HashRouter feature

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import MovieProvider from "./context/MovieProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename="/">
       <MovieProvider>
         <App />
       </MovieProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
